2ea9ed35e46f624ccedf6ff7eacc75e016ceeae4
huzaifa <muhammadhuzaifakhalidaziz@outlook.com>
2025-05-05 15:22:21 +0400

    Update config.js

--- config.js
+++ config.js
@@ -14,3 +14,10 @@ module.exports = {
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
 }
+{
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  3543,
+      secure: false
+    }
