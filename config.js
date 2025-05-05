ef8e01dbdef015e7add68efcdf85172fb3c02c8d
huzaifa <muhammadhuzaifakhalidaziz@outlook.com>
2025-05-05 12:19:19 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,23 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-  ownerId: '1004206704994566164',
+  ownerId: '843061674378002453',
   status: {
     rotateDefault: [
-      { name: 'Netflix', type: ActivityType.Watching },
-      { name: 'GTA VI', type: ActivityType.Playing },
-      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.twitch.tv/glaceytt' },
-      { name: 'Spotify', type: ActivityType.Listening },
+      { name: 'Students!', type: ActivityType.helping },
+      { name: 'Fortnite', type: ActivityType.Playing },
+      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.youtube.com/@ajirotech' },
+      { name: 'to xcho_', type: ActivityType.Listening },
     ],
     songStatus: true
   },
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
 }
+{
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  3543,
+      secure: false
+    }
