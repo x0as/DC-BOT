d2c1ec5f400bb83bf7d2b7d460972afa735f1e6c
huzaifa <muhammadhuzaifakhalidaziz@outlook.com>
2025-05-06 21:02:00 +0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   ownerId: '843061674378002453',
   status: {
     rotateDefault: [
-      { name: 'Students', type: ActivityType.helping },
+      { name: 'Students work', type: ActivityType.watching },
       { name: 'Fortnite', type: ActivityType.Playing },
       { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.youtube.com/@ajirotech' },
       { name: 'to xcho_', type: ActivityType.Listening },
